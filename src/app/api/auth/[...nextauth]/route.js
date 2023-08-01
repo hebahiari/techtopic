518642ddb618761418e2a1ddc7705a2e0c46ae59
Heba <hebahiary+github@gmail.com>
2023-08-01 16:07:37 -0700

    added sign in

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "@/utils/db";
+import bcrypt from "bcryptjs";
 
 export const authOptions = NextAuth({
   // Configure one or more authentication providers
@@ -8,8 +11,38 @@ export const authOptions = NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connect();
+
+        try {
+          const user = await User.findOne({ email: credentials.email });
+
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error("Wrong Password");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { authOptions as GET, authOptions as POST };
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,11 +1,41 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Login = () => {
+  const [error, setError] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const email = event.target[0].value;
+    const password = event.target[1].value;
+
+    signIn("credentials", { email, password });
+  };
+
   return (
     <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className={styles.input}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.input}
+          required
+        />
+        <button className={styles.button}>Login</button>
+        <Link href="/dashboard/login">Login with an existing account</Link>
+      </form>
+      {error && "something went wrong"}
       <button onClick={() => signIn("google")}>Login with Google</button>
     </div>
   );
